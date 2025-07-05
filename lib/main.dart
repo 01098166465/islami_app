e0a17489dbbfe591fac115a607c0bb22a44b0674
Mahmoud Khaled <christogram105@gmail.com>
2025-07-05 16:24:38 +0300

    create intro screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/app_theme.dart';
 import 'package:islami_app/home_screen.dart';
+<<<<<<< HEAD
 import 'package:islami_app/tabs/quran/sura_details_screen.dart';
+=======
+import 'package:islami_app/intro_screen.dart';
+>>>>>>> feature/intro
 
 void main() {
   runApp(IslamiApp());
@@ -12,10 +16,18 @@ class IslamiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+<<<<<<< HEAD
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
         SuraDetailsScreen.routrName: (context) => SuraDetailsScreen(),
+=======
+      initialRoute: IntroScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+
+        IntroScreen.routeName: (context) => IntroScreen(),
+>>>>>>> feature/intro
       },
       theme: AppTheme.lightThem,
       darkTheme: AppTheme.darkThem,
