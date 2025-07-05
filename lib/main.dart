2e97959fd935a9d0d996fbe64a993bd2a5763a85
Mahmoud Khaled <christogram105@gmail.com>
2025-07-05 16:35:50 +0300

    clear main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/app_theme.dart';
 import 'package:islami_app/home_screen.dart';
-<<<<<<< HEAD
+
 import 'package:islami_app/intro_screen.dart';
-=======
 
 import 'package:islami_app/tabs/quran/sura_details_screen.dart';
-=======
+
 import 'package:islami_app/intro_screen.dart';
->>>>>>> feature/intro
->>>>>>> development
 
 void main() {
   runApp(IslamiApp());
@@ -20,24 +17,12 @@ class IslamiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-      initialRoute: HomeScreen.routeName,
-      routes: {
-        HomeScreen.routeName: (context) => HomeScreen(),
-        SuraDetailsScreen.routrName: (context) => SuraDetailsScreen(),
-=======
->>>>>>> development
+
       initialRoute: IntroScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
 
         IntroScreen.routeName: (context) => IntroScreen(),
-<<<<<<< HEAD
-=======
->>>>>>> feature/intro
->>>>>>> development
       },
       theme: AppTheme.lightThem,
       darkTheme: AppTheme.darkThem,
